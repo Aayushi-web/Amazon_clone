586007a82f1dc1cccfaae08bb596bfad04216f5a
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-08-01 12:47:56 +0530

    assets in index.js

---
+++ amazon/src/assets/index.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.png'
+import bannerImgOne from './bannerImgOne.jpg'
+import bannerImgTwo from './bannerImgTwo.jpg'
+import bannerImgThree from './bannerImgThree.jpg'
+import bannerImgFour from './bannerImgFour.jpg'
+import bannerImgFive from './bannerImgFive.jpg'
+import flag from './flag.webp'
+import emptyCart from './emptyCart.png'
+export{
+    logo,bannerImgFive,bannerImgOne,bannerImgThree,flag,bannerImgFour,bannerImgTwo
+}
